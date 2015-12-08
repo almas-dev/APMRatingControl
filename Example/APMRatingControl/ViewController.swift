2d4ac145ffbf6fbe2aeeac14236e403d3ac4a08d
Alexander Maslennikov <almas.dev@gmail.com>
2015-12-08 13:50:50 +0300

    Add rate logic

--- Example/APMRatingControl/ViewController.swift
+++ Example/APMRatingControl/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import APMRatingControl
 
 class ViewController: UIViewController {
+    let fixedRating = APMRatingControl()
     let ratingControl = APMRatingControl()
 
     override func viewDidLoad() {
@@ -17,6 +18,22 @@ class ViewController: UIViewController {
 
         view.backgroundColor = UIColor(white: 0.23, alpha: 1.0)
 
+
+        fixedRating.rating = 3.33
+        fixedRating.maximumValue = 8
+        fixedRating.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fixedRating)
+        view.addConstraints([
+                NSLayoutConstraint(item: fixedRating, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: fixedRating, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 0.5, constant: 0),
+                NSLayoutConstraint(item: fixedRating, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 132),
+                NSLayoutConstraint(item: fixedRating, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20)
+        ])
+
+
+        ratingControl.canRate = true
+        ratingControl.stepRate = 1
+        ratingControl.addTarget(self, action: "ratingControlChangeValueAction:", forControlEvents: .ValueChanged)
         ratingControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(ratingControl)
         view.addConstraints([
@@ -25,6 +42,10 @@ class ViewController: UIViewController {
                 NSLayoutConstraint(item: ratingControl, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 132),
                 NSLayoutConstraint(item: ratingControl, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20)
         ])
+    }
+
+    func ratingControlChangeValueAction(sender: APMRatingControl) {
+        print("\(sender.rating)")
     }
 }
 
--- Pod/Classes/APMRatingControl.swift
+++ Pod/Classes/APMRatingControl.swift
@@ -6,6 +6,14 @@
 import UIKit
 
 public class APMRatingControl: UIControl {
+    public var rating: CGFloat = 0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    public var maximumValue: UInt = 5
+    public var canRate: Bool = false
+    public var stepRate: CGFloat = 0
 
     required public init(coder aDecoder: NSCoder) {
         fatalError("NSCoding not supported")
@@ -17,18 +25,61 @@ public class APMRatingControl: UIControl {
         backgroundColor = UIColor.clearColor()
     }
 
+    override public func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        if canRate {
+            handleTouch(touch)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override public func continueTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        handleTouch(touch)
+        return true
+    }
+
+    override public func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
+        sendActionsForControlEvents(.ValueChanged)
+    }
+
+    func handleTouch(touch: UITouch) {
+        let starPlaceWidth = bounds.size.width / CGFloat(maximumValue)
+        let location = touch.locationInView(self)
+        let locRating: CGFloat
+        if location.x >= 0 {
+            locRating = location.x / starPlaceWidth
+        } else {
+            locRating = 0
+        }
+
+        if stepRate > 0 {
+            var roundRating = round(locRating / stepRate) * stepRate
+            if roundRating < locRating {
+                roundRating += stepRate
+            }
+            updateRating(roundRating)
+        } else {
+            updateRating(locRating)
+        }
+    }
+
+    func updateRating(rating: CGFloat) {
+        if rating != self.rating && rating >= 0 && rating <= CGFloat(maximumValue) {
+            self.rating = rating
+        }
+    }
+
     override public func drawRect(rect: CGRect) {
         let spacing: CGFloat = 5
-        let maximumValue: CGFloat = 5
-        let value: CGFloat = 3.33
 
-        let availableWidth = rect.size.width - (spacing * (maximumValue - 1)) - 2
-        let cellWidth = (availableWidth / maximumValue)
+        let availableWidth = rect.size.width - (spacing * (CGFloat(maximumValue) - 1)) - 2
+        let cellWidth = (availableWidth / CGFloat(maximumValue))
         let starSide = (cellWidth <= rect.size.height) ? cellWidth : rect.size.height - 2
-        for var idx: CGFloat = 0; idx < maximumValue; idx++ {
+        for var idx: CGFloat = 0; idx < CGFloat(maximumValue); idx++ {
             let center = CGPointMake(cellWidth * idx + cellWidth / 2 + spacing * idx + 1, rect.size.height / 2)
             let frame = CGRectMake(center.x - starSide / 2, center.y - starSide / 2, starSide, starSide)
-            drawStarShapeWithFrame(frame, tintColor: UIColor.whiteColor(), progress: value - idx)
+            drawStarShapeWithFrame(frame, tintColor: UIColor.whiteColor(), progress: rating - idx)
         }
     }
 
