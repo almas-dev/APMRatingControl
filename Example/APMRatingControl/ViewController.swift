04cb42fe3c4e13c5dc7e94c0790febf6dde54df2
Alexander Maslennikov <almas.dev@gmail.com>
2015-12-13 21:15:55 +0300

    Fix example

--- Example/APMRatingControl/ViewController.swift
+++ Example/APMRatingControl/ViewController.swift
@@ -12,6 +12,7 @@ import APMRatingControl
 class ViewController: UIViewController {
     let fixedRating = APMRatingControl()
     let ratingControl = APMRatingControl()
+    let floatRatingControl = APMRatingControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +34,10 @@ class ViewController: UIViewController {
 
 
         ratingControl.canRate = true
-        //ratingControl.stepRate = 0.5
+        ratingControl.stepRate = 0.5
         ratingControl.borderColor = UIColor.whiteColor()
         ratingControl.fillColor = UIColor.whiteColor()
         ratingControl.addTarget(self, action: "ratingControlChangeValueAction:", forControlEvents: .ValueChanged)
-        ratingControl.backgroundColor = UIColor.brownColor()
         ratingControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(ratingControl)
         view.addConstraints([
@@ -45,6 +45,22 @@ class ViewController: UIViewController {
                 NSLayoutConstraint(item: ratingControl, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0),
                 NSLayoutConstraint(item: ratingControl, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 132),
                 NSLayoutConstraint(item: ratingControl, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20)
+        ])
+
+
+        floatRatingControl.canRate = true
+        floatRatingControl.borderWidth = 1
+        floatRatingControl.borderColor = UIColor.whiteColor()
+        floatRatingControl.fillColor = UIColor.whiteColor()
+        floatRatingControl.addTarget(self, action: "ratingControlChangeValueAction:", forControlEvents: .ValueChanged)
+        floatRatingControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.1)
+        floatRatingControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(floatRatingControl)
+        view.addConstraints([
+                NSLayoutConstraint(item: floatRatingControl, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+                NSLayoutConstraint(item: floatRatingControl, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.5, constant: 0),
+                NSLayoutConstraint(item: floatRatingControl, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 264),
+                NSLayoutConstraint(item: floatRatingControl, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30)
         ])
     }
 
